20c106ff68de423cf6936de46ac1c4a91f8165e9
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-29 12:56:35 +0300

    fixyed homepage

---
+++ src/components/loader/loader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './loader.scss';
+
+const Loader = () => (
+    <div className="loader-wrapper"><div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
+)
+export default Loader;
--- src/page/homePage/homePage.js
+++ src/page/homePage/homePage.js
@@ -3,50 +3,42 @@ import Information from './../../components/information/information';
 import Feeds from './../../components/feeds/feeds';
 import { CurrentUserContext } from './../../context/currentUser/currentUserContext';
 import { useFetch } from './../../library/hooksLibrary';
+import Loader from './../../components/loader/loader';
+import BackendErrors from './../../components/backendErrors/backendErrors';
 
 const HomePage = props => {
+    const feedUrl = '/articles/feed?limit=10&offset=0';
+    const globalFeedUrl = '/articles?limit=10&offset=0';
     const [user] = useContext(CurrentUserContext);
-    const [tag, setTag] = useState('');
-    const [isMyFeed, setIsMyFeed] = useState(user.isLoggedIn);
-    const articlesUrl = isMyFeed ? '/articles/feed?limit=10&offset=0' : '/articles?limit=10&offset=0';
-    const [articleUrl, setArticleUrl] = useState(articlesUrl);
-    const [{response:tags}, doFetchTags] = useFetch('/tags');
-    const [{response:articles}, doFetchArtciles] = useFetch(articleUrl);
-    
-    
+    const [articleUrl, setArticleUrl] = useState(user.isLoggedIn ? feedUrl : globalFeedUrl);
+    const [{response:tags, error:errorTags}, doFetchTags] = useFetch('/tags');
+    const [{response:articles, error:errorArticles, isLoading:isLoadingArticles, setResponse}, doFetchArtciles] = useFetch(articleUrl);
+
     const tabsLink = [
-        {text: 'Your Feed', href: user.isLoggedIn ? '/' : '/login', exact: true, onClick:() => {setIsMyFeed(true);setArticleUrl('/articles/feed?limit=10&offset=0');setTag('');}},
-        {text: 'Global Feed', href: '#', onClick:() => {setIsMyFeed(false);setArticleUrl('/articles?limit=10&offset=0');setTag('');}}
+        {text: 'Your Feed', href: user.isLoggedIn ? '/' : '/login', exact: true, onClick: () => {
+            setArticleUrl(feedUrl);
+            setResponse(null);
+            doFetchArtciles();
+        }},
+        {text: 'Global Feed', href: '#', onClick: () =>  {
+            setArticleUrl(globalFeedUrl)
+            setResponse(null);
+            doFetchArtciles();
+        }}
     ];
-    
+
     useEffect(() => {
         doFetchTags();
+        doFetchArtciles();
     },[]);
-
-    useEffect(() => {
-        if(!articles) return;
-        if(!tag) doFetchArtciles();
-    }, [tag, doFetchArtciles])
-
-    useEffect(() => {
-        doFetchArtciles();
-    },[isMyFeed, doFetchArtciles])
-
-    function tagsClickHandler(name) {
-        setArticleUrl(`/articles?tag=${name}&limit=10&offset=0`);
-        setTag(name);
-        doFetchArtciles();
-    }
- 
-    if(tag) {
-        tabsLink.push({text:`#${tag}`, href:'/'})
-    }
 
  
     return (
         <>
             {user.isLoading || user.isLoggedIn ? null : <Information title="medium" subtitle="A place to share your React knowledge"/>}
-            {articles && tags ? <Feeds tabsLink={tabsLink} onTagsClickHandler={tagsClickHandler} tagsList={tags.tags} articleList={articles.articles}/> :  <p style={{textAlign: 'center'}}>Loading...</p>}
+            {articles  && tags ? <Feeds tabsLink={tabsLink} tagsList={tags.tags} articleList={articles.articles}/> :  null}
+            {isLoadingArticles ? <Loader /> : null}
+            {errorTags || errorArticles ? <BackendErrors errors={errorTags.errors || errorArticles.errors} /> : null}
         </>
     )
 }
